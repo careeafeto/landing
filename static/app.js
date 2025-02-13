68b71283b6073d08db7d27eeaf6254458a8d304c
Danilo Moura <danilolmoura@gmail.com>
2025-02-13 13:58:26 -0300

    update: add message to whatsapp button

---
+++ static/app.js
@@ -0,0 +1,17 @@
+function enviarMensagemWhatsApp(tipo) {
+    let numero = "5521978853576";
+    let mensagem = "";
+
+
+    if (tipo === "solicitar") {
+        mensagem = "Olá! Gostaria de saber mais sobre os serviços disponíveis na Care e Afeto.";
+    } else if (tipo === "trabalhar") {
+        mensagem = "Olá! Tenho interesse em me tornar um cuidador na Care e Afeto.";
+    }
+
+    let url = `https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`;
+    window.open(url, "_blank");
+}
+
+
+
